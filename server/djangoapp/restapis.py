0870afa0c1694e7c1cc006b35bf3ff4d93f6e2a8
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-01-15 06:43:02 -0500

    ibm cloud deploy

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,7 +10,7 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-def get_request(endpoint, **kwargs):
+ddef get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
         for key,value in kwargs.items():
@@ -25,7 +25,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except:
         # If any error occurs
-        print("Network exception occurred") 
+        print("Network exception occurred")
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
