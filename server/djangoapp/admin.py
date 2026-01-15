90afd6fd9c4a6142bcf4ce775e28e02cd510d0e4
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-01-15 16:19:44 +0100

    Fix flake8 formatting issues in djangoapp models, populate, admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,20 +3,21 @@ from .models import CarMake, CarModel
 
 
 class CarModelInline(admin.TabularInline):
-	model = CarModel
-	extra = 0
+    model = CarModel
+    extra = 0
 
 
 @admin.register(CarModel)
 class CarModelAdmin(admin.ModelAdmin):
-	list_display = ('name', 'car_make', 'dealer_id', 'type', 'year')
-	list_filter = ('car_make', 'type', 'year')
-	search_fields = ('name',)
+    list_display = ("name", "car_make", "dealer_id", "type", "year")
+    list_filter = ("car_make", "type", "year")
+    search_fields = ("name",)
 
 
 @admin.register(CarMake)
 class CarMakeAdmin(admin.ModelAdmin):
-	list_display = ('name', 'country', 'website')
-	search_fields = ('name',)
-	inlines = [CarModelInline]
+    list_display = ("name", "country", "website")
+    search_fields = ("name",)
+    inlines = [CarModelInline]
+
 
